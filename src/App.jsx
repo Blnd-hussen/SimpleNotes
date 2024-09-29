9d8752df1588f22ebfcd0057cb33ba10cd7b3d3c
Blnd-hussen <blndqaladzey12@gmail.com>
2024-09-29 10:17:06 +0300

    Add new note form popup && cancelation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Header from './components/Header/Header'
-import Notes from './components/Notes/Notes'
-import AddForm from './components/AddForm/AddForm'
+import Header from "./components/Header/Header";
+import Notes from "./components/Notes/Notes";
+import AddForm from "./components/AddForm/AddForm";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   const [hasAddForm, setHasAddForm] = useState(false);
+  const [fromClosed, setFormClosed] = useState(false);
 
   const handleAddClick = () => {
-    setHasAddForm(hasAddForm => !hasAddForm);
-  }
+    setHasAddForm((prev) => !prev);
+    setFormClosed(false);
+  };
+
+  const handleFormClose = () => {
+    setHasAddForm(false);
+    setFormClosed(true);
+  };
 
   return (
     <main>
-      <Header onAddClick={handleAddClick}/>
-      { hasAddForm && <AddForm /> }
+      <Header onAddClick={handleAddClick} />
+      {hasAddForm && !fromClosed && <AddForm onClose={handleFormClose} />}
       <Notes />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,19 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import browser from "webextension-polyfill";
 
 import "./AddForm.css";
 
-function AddForm() {
-  const [formData, setFormData] = useState({ title: "" }, { body: "" });
+function AddForm(props) {
+  const [formData, setFormData] = useState({ title: "", body: "" });
+  const [disabled, setDisabled] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
+
+    const action = event.nativeEvent.submitter.name;
+
+    if (action === "save") {
+      console.log("save is not implemeanted yet");
+      props.onClose();
+    } else if (action === "cancel") {
+      props.onClose();
+    }
   };
+
+  const getDate = () => {
+    const date = new Date()
+      .toJSON()
+      .slice(0, 10)
+      .split("-")
+      .reverse()
+      .join("-");
+
+    const [hour, AMPM] = new Date()
+      .toLocaleString("en-US", {
+        hour: "numeric",
+        hour12: true,
+      })
+      .split(" ");
+
+    const minute = new Date().toLocaleString("en-US", {
+      minute: "numeric",
+      hour12: true,
+    });
+
+    return `${date} - ${hour}:${minute} ${AMPM}`;
+  };
+
+  useEffect(() => {
+    setDisabled(formData.title.trim() === "");
+  }, [formData]);
 
   return (
     <div className="form-container">
       <form className="form-note" onSubmit={handleSubmit}>
-        <div className="form-note__timestamp">{new Date().getFullYear()}</div>
+        <div className="form-note__timestamp">{getDate()}</div>
         <input
           type="text"
           placeholder="Title..."
@@ -21,6 +58,7 @@ function AddForm() {
           autoFocus
           value={formData.title}
           onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+          className={disabled ? "disabled-input" : ""}
         />
         <textarea
           placeholder="body..."
@@ -29,8 +67,19 @@ function AddForm() {
         ></textarea>
         <hr />
         <div className="form-note__auctions">
-          <button className="form-note__auctions-save" type="submit">Save</button>
-          <button className="form-note__auctions-cancel">cancel</button>
+          <button
+            className={`form-note__auctions-save ${
+              disabled ? "disabled-button" : ""
+            }`}
+            type="submit"
+            name="save"
+            disabled={disabled}
+          >
+            Save
+          </button>
+          <button className="form-note__auctions-cancel" name="cancel">
+            cancel
+          </button>
         </div>
       </form>
     </div>
